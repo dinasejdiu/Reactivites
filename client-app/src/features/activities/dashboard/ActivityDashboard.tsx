23effc92a8836e44b89e8986f3830507e5e1e99c
Erodina Sejdiu <es49107@ubt-uni.net>
2021-09-10 01:27:54 +0200

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Grid} from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 
@@ -25,7 +26,7 @@ if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/
              <ActivityList />
        </Grid.Column>
        <Grid.Column width='6'>
-           <h2>Activity filters</h2>
+          <ActivityFilters/>
        </Grid.Column>
     </Grid>
 
